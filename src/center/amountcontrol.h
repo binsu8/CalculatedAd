61a65a88b0f30c3980091a5a72d9f8b0beb6a9c7
binsu8 <binsu8@163.com>
2014-03-21 18:03:10 +0800

    update

---
+++ src/center/amountcontrol.h
@@ -0,0 +1,20 @@
+#ifndef _AMOUNTCONTROL_
+#define _AMOUNTCONTROL_
+
+#include <string>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class AmountControl {
+
+public:
+    AmountControl();
+
+
+
+
+
+}
+
